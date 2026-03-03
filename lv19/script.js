eee9cb3448c50ca11c103c27fd2cbd08ac500e0d
mali1sunjo <135636352+mali1sunjo@users.noreply.github.com>
2026-03-03 07:54:43 +0100

    ASDAS

--- lv19/script.js
+++ lv19/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    // Zadatak 1 – Sakrij / Prikaži
     $("#sakrij").click(function(){
         $("#tekst").hide();
     });
@@ -8,16 +9,16 @@ $(document).ready(function(){
         $("#tekst").show();
     });
 
-
+    // Zadatak 2 – Toggle i boja
     $("#toggle").click(function(){
         $("#tekst").toggle();
     });
 
     $("#boja").click(function(){
-        $("#tekst").addClass("crveno");
+        $("#tekst").toggleClass("crveno");
     });
 
-
+    // Zadatak 3 – Animacije
     $("#fade").click(function(){
         $("#tekst").fadeToggle();
     });
@@ -26,26 +27,26 @@ $(document).ready(function(){
         $("#tekst").slideToggle();
     });
 
-
+    // Zadatak 4 – Promjena sadržaja
     $("#promijeniTekst").click(function(){
         let noviTekst = $("#unos").val();
         $("#tekst").text(noviTekst);
     });
 
-
-    
+    // Mini projekt – Kartica
     $("#kartica").click(function(){
         $(this).css("background","lightblue");
     });
 
-  
-    $("#kartica").hover(function(){
-        $(this).addClass("velika");
-    }, function(){
-        $(this).removeClass("velika");
-    });
+    $("#kartica").hover(
+        function(){
+            $(this).addClass("velika");
+        },
+        function(){
+            $(this).removeClass("velika");
+        }
+    );
 
-  
     $("#sakrijKarticu").click(function(e){
         e.stopPropagation();
         $("#kartica").fadeOut();
