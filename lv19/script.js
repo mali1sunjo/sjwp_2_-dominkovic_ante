ba102ed48a586867ef48658fc57cd281a3be8f29
mali1sunjo <135636352+mali1sunjo@users.noreply.github.com>
2026-03-17 07:57:28 +0100

    asd

--- lv19/script.js
+++ lv19/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    // Zadatak 1 – Sakrij / Prikaži
+  
     $("#sakrij").click(function(){
         $("#tekst").hide();
     });
@@ -9,7 +9,7 @@ $(document).ready(function(){
         $("#tekst").show();
     });
 
-    // Zadatak 2 – Toggle i boja
+ 
     $("#toggle").click(function(){
         $("#tekst").toggle();
     });
@@ -18,7 +18,6 @@ $(document).ready(function(){
         $("#tekst").toggleClass("crveno");
     });
 
-    // Zadatak 3 – Animacije
     $("#fade").click(function(){
         $("#tekst").fadeToggle();
     });
@@ -27,13 +26,12 @@ $(document).ready(function(){
         $("#tekst").slideToggle();
     });
 
-    // Zadatak 4 – Promjena sadržaja
     $("#promijeniTekst").click(function(){
         let noviTekst = $("#unos").val();
         $("#tekst").text(noviTekst);
     });
 
-    // Mini projekt – Kartica
+   
     $("#kartica").click(function(){
         $(this).css("background","lightblue");
     });
